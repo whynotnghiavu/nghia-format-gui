b17fbb2d5e870aebc0be1e7acaad5907c814a835
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-27 02:03:10 +0700

    update nghia-format-gui.py

--- contents/nghia-format-gui.py
+++ contents/nghia-format-gui.py
@@ -8,7 +8,6 @@ import pyautogui
 
 def format(directory):
     exclude_dirs = [".git", "node_modules", "__pycache__"]
-
     for root, dirs, files in os.walk(directory):
         dirs[:] = [d for d in dirs if d not in exclude_dirs]
 
@@ -18,41 +17,43 @@ def format(directory):
             if file.endswith(".code-workspace"):
                 continue
 
-            file_path = os.path.join(root, file)
-            print(f"Processing file: {file_path}")
-            subprocess.run(["code", file_path], check=True, shell=True)
-            sleep(1)
+            # file_path = os.path.join(root, file)
+            # print(f"Processing file: {file_path}")
+            # subprocess.run(["code", file_path], check=True, shell=True)
+            # sleep(1)
 
-            if os.name == 'nt':
-                pyautogui.hotkey('alt', 'shift', 'f')
-            elif os.name == 'posix':
-                pyautogui.hotkey('alt', 'shift', 'o')
-                pyautogui.hotkey('ctrl', 'shift', 'i')
-            sleep(1)
+            # if os.name == 'nt':
+            #     pyautogui.hotkey('alt', 'shift', 'f')
+            # elif os.name == 'posix':
+            #     pyautogui.hotkey('alt', 'shift', 'o')
+            #     pyautogui.hotkey('ctrl', 'shift', 'i')
+            # sleep(1)
 
-            # if file.endswith(".py"):
-            #     file_path = os.path.join(root, file)
-            #     print(f"Processing file: {file_path}")
-            #     subprocess.run(["code", file_path], check=True, shell=True)
-            #     sleep(1)
+            if file.endswith(".py"):
+                file_path = os.path.join(root, file)
+                print(f"Processing file: {file_path}")
+                subprocess.run(["code", file_path], check=True, shell=True)
+                sleep(1)
 
-            #     if os.name == 'nt':
-            #         pyautogui.hotkey('alt', 'shift', 'f')
-            #     elif os.name == 'posix':
-            #         pyautogui.hotkey('alt', 'shift', 'o')
-            #         pyautogui.hotkey('ctrl', 'shift', 'i')
-            #     sleep(1)
+                if os.name == 'nt':
+                    pyautogui.hotkey('alt', 'shift', 'f')
+                elif os.name == 'posix':
+                    pyautogui.hotkey('alt', 'shift', 'o')
+                    pyautogui.hotkey('ctrl', 'shift', 'i')
+                sleep(1)
 
-
-list_code = [
-    # "/home/vvn20206205/Desktop/einvoice-system/microservices/api-gateway/src",
-    # "/home/vvn20206205/Desktop/einvoice-system/microservices/user-service/src",
-    # "/home/vvn20206205/Desktop/einvoice-system/microservices/invoice-service/src",
-    # "/home/vvn20206205/Desktop/einvoice-system/microservices/tct-demo/src",
-    # "/home/vvn20206205/Desktop/vuvannghia/src",
-    # "/home/vvn20206205/Desktop/einvoice-system/microservices/report-service/src",
-    # "C:\\Users\\vvn20206205\\Downloads\\Nghia\\Git\\whynotnghiavu",
-    "C:\\Users\\vvn20206205\\Downloads\\Nghia\\Git\\whynotnghiavu\\nghia-contact-manager",
-]
-for code in list_code:
-    format(code)
+if __name__ == "__main__":
+    paths = [
+        # "/home/vvn20206205/Desktop/einvoice-system/microservices/api-gateway/src",
+        # "/home/vvn20206205/Desktop/einvoice-system/microservices/user-service/src",
+        # "/home/vvn20206205/Desktop/einvoice-system/microservices/invoice-service/src",
+        # "/home/vvn20206205/Desktop/einvoice-system/microservices/tct-demo/src",
+        # "/home/vvn20206205/Desktop/vuvannghia/src",
+        # "/home/vvn20206205/Desktop/einvoice-system/microservices/report-service/src",
+        # "C:\\Users\\vvn20206205\\Downloads\\Nghia\\Git\\whynotnghiavu",
+        # "C:\\Users\\vvn20206205\\Downloads\\Nghia\\Git\\whynotnghiavu\\nghia-contact-manager",
+        # r"C:\Users\vvn20206205\Downloads\Nghia\Git\whynotnghiavu\nghia-extensions-video-VN\contents",
+        r"C:\Users\vvn20206205\Downloads\Nghia\Git\whynotnghiavu\VideoVN",
+    ]
+    for path in paths:
+        format(path)
